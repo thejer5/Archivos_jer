c94f20e5fcf60c81b3fcfc11812901e6183a0d15
thejer5 <thejer19@gmail.com>
2020-02-07 16:06:29 -0400

    Se agrego un codigo recursivo

--- codigosuma.py
+++ codigosuma.py
@@ -1,4 +1,9 @@
-def sumar(n):
-   return(n + 2)
-print(sumar(9))
-nbfhwjejfjihejkf
+def collatz(n):
+   print(int(n))
+   if n == 1:
+      return 1
+   if n % 2 == 0:
+      collatz(n/2)
+   else:
+      collatz(n * 3 + 1)
+print(collatz(100))
