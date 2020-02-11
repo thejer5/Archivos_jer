10007d8009e29fe10e4a2b4e86b6871b878c617f
thejer5 <thejer19@gmail.com>
2020-02-11 15:27:44 -0400

    Proceso cambiado

--- codigosuma.py
+++ codigosuma.py
@@ -1,10 +1,23 @@
-print('CONJENTURA DE COLLATZ')
-def collatz(n):
-   print(int(n))
-   if n == 1:
-      return 1
-   if n % 2 == 0:
-      collatz(n/2)
-   else:
-      collatz(n * 3 + 1)
-print(collatz(100))
+# resultado = lambda valor1, valor2 : valor1 / valor2
+# value = resultado(20,4)
+# print(value)
+
+# formato = lambda question : '.......{}.......'.format(question)
+# resultado = formato('[ ]')
+# print(resultado)
+# print(resultado)
+# print(resultado)
+# print(resultado)
+# def verifier(value1, value2): ####VERIFICA SI CADA VALOR ES MAYOR A 0 Y SI NO RETORNA FALSE
+#    return(value1 > 0 and value2 > 0)
+# def multi(value1, value2):
+#    if verifier(value1, value):#LLAMADO DE LA FUNCION
+#       return(value1 * value2)
+# result = verifier(9,7)
+# print(result)
+def division(v1, v2):
+   def verifier(v1, v2):
+      return v1 > 0 and v2 > 0
+   return v1 / v2
+result = division(50, 3)
+print(result)
